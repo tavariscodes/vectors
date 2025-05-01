8242879257415f49bfe1322368d504b412b5a89a
tavariscodes <tavariscodes@gmail.com>
2025-05-01 18:59:10 -0500

    use rect.left.. instead of width/height

--- src/main.rs
+++ src/main.rs
@@ -29,40 +29,37 @@ impl Default for MyBall {
             x: 100.0,
             y: 100.0,
             xspeed: 2.5,
-            yspeed: 2.0
+            yspeed: 2.0,
         }
     }
 }
 
-// A Euclidean Vector also known as the geometric vector is defined as 
+// A Euclidean Vector also known as the geometric vector is defined as
 // an entity that has both magnitude and direction. Typically drawn as an arrow
 
 // Magnitude is = length of Vector
 
 impl eframe::App for MyBall {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-
         egui::CentralPanel::default().show(ctx, |ui| {
             let rect = ui.max_rect();
             let painter = ui.painter();
-            painter.rect_filled(rect, 0.0, Color32::WHITE);            
+            painter.rect_filled(rect, 0.0, Color32::WHITE);
 
             self.x += self.xspeed;
             self.y += self.yspeed;
 
-
-            if self.x > rect.width() || self.x < 0.0 {
+            if self.x > rect.right() || self.x < rect.left() {
                 self.xspeed *= -1.0;
             }
 
-            if self.y > rect.height() || self.y < 0.0 {
+            if self.y > rect.bottom() || self.y < rect.top() {
                 self.yspeed *= -1.0;
             }
 
             painter.circle_filled(Pos2::new(self.x, self.y), 20.0, Color32::GRAY);
 
             ctx.request_repaint(); // Force continuous animation
-
         });
     }
 }
